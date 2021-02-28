88538675bd173fb7485b73cc2df152b2aa0612c8
Chadwell-Clark <wcclark23@comcast.net>
2021-02-28 16:43:59 -0600

    added JournalEntry and JournalEntryList.js created modules to render entries back to the DOM below form

--- scripts/JournalData.js
+++ scripts/JournalData.js
@@ -20,9 +20,25 @@ const journal = [
         id: 2,
         date: "07/31/27",
         concept: "Spacetime manipulation",
-        entry: "We talked about manipulation of spacetime modalities via electrochemical focus of inner quanta photonic emission.",
+        entry: "We talked about manipulation of spacetime modalities via electrochemical crystalization pressure transference of inner quanta photonic emission.",
         quote: "It's all devistatingly true--except the bits that are lies --Douglas Adams--",
-        mood: "Partly Sunny"
+        mood: "Sunny"
+    },
+    {
+        id: 3,
+        date: "08/14/03",
+        concept: "Flexbox",
+        entry: "Found that the best kind of box is flexbox, cause it flexes. Then some one grid boxed me into oblivion.",
+        quote: "C'mon man. Cornpop was a bad dude. --Joe Biden--",
+        mood: "Partly Cloudy"
+    },
+    {
+        id: 4,
+        date: "11/4/16",
+        concept: "Covfefe",
+        entry: "Shrugs shoulders",
+        quote: "Despite the negative press covfefe",
+        mood: "Gloom and Doom"
     }
 ]
 
